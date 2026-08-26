5697cd8944426ad566194cf317153c5ff58ff323
egieirwn <notnpc1155@gmail.com>
2026-08-26 18:40:14 +0700

    Move Firebase keys to environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAz6WpoMwCK2mNzZwjJ8LE5JvN77F73QgY",
-  authDomain: "logbook-apps.firebaseapp.com",
-  projectId: "logbook-apps",
-  storageBucket: "logbook-apps.firebasestorage.app",
-  messagingSenderId: "711703839215",
-  appId: "1:711703839215:web:2f8e84b3116b96519cb61a",
-  measurementId: "G-KC1P53R5KN"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
